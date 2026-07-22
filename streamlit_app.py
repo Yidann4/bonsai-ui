2b815a4d9918ce620fef3cc3f81d9982698c63b2
Yidann <bio.aidan@gmail.com>
2026-07-22 20:31:57 +1000

    iphone sizing

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,16 @@ page_bg_img = f'''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
 STORE_SESSION_KEY = "water_levels_store"
-
-if st.button("Refresh data"):
-    clear_water_levels_store_cache()
-    st.session_state.pop(STORE_SESSION_KEY, None)
 
 st.session_state[STORE_SESSION_KEY] = load_water_levels_store(postgres_conn)
 store = st.session_state[STORE_SESSION_KEY]
 
-store = st.session_state[STORE_SESSION_KEY]
+if st.button("Refresh data"):
+    with st.spinner("Refreshing data..."):
+        clear_water_levels_store_cache()
+        st.session_state.pop(STORE_SESSION_KEY, None)
+    st.rerun()
 
 from src.components.moisture_card import moisture_card
 moisture_card(store.latest_water_level, store.latest_battery_level, store.latest_measured_time, store.latest_watered_time)
